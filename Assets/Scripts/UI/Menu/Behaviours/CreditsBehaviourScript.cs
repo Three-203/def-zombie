a41c9c9f23d7afb050ae0cc77a310b0c92045697
ThoNguyen <thont.ctv@gmail.com>
2023-04-13 00:21:11 +0700

    first commit

---
+++ Assets/Scripts/UI/Menu/Behaviours/CreditsBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsBehaviourScript : StateMachineBehaviour
+{
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        bool winGame = animator.transform.parent.GetComponent<CreditsScript>().winGame;
+
+        if (!winGame) animator.transform.parent.parent.GetChild(0).gameObject.SetActive(true);
+
+        animator.transform.parent.gameObject.SetActive(false);
+    }
+}
